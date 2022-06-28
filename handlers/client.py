ce3e845e81306952bca91135ff3a1e995789afa5
artoshka <105043407+artoshka@users.noreply.github.com>
2022-06-28 14:13:05 +0300

    hw6

--- handlers/client.py
+++ handlers/client.py
@@ -1,5 +1,6 @@
 from aiogram import types, Dispatcher
 from aiogram.types import ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
+from parser import series, anime, cartoons
 
 from config import bot
 
@@ -47,8 +48,50 @@ async def pin(messege: types.Message):
     else:
         await bot.pin_chat_message(messege.chat.id, messege.message_id)
 
+async def parser_series(message: types.Message):
+    data = series.parser()
+    for movie in data:
+        desc = movie['desc'].split(', ')
+        await bot.send_message(
+            message.from_user.id,
+            f"{movie['title']}\n"
+            f"Год: {desc[0]}\n"
+            f"Город: {desc[1]}\n"
+            f"Жанр: #{desc[2]}\n\n"
+            f"{movie['link']}"
+         )
+
+async def parser_cartoons(message: types.Message):
+    data = cartoons.parser()
+    for cartoon in data:
+        desc = cartoon['desc'].split(', ')
+        await bot.send_message(
+            message.from_user.id,
+            f"{cartoon['title']}\n"
+            f"Год: {desc[0]}\n"
+            f"Город: {desc[1]}\n"
+            f"Жанр: #{desc[2]}\n\n"
+            f"{cartoon['link']}"
+         )
+
+async def parser_anime(message: types.Message):
+    data = anime.parser()
+    for animation in data:
+        desc = animation['desc'].split(', ')
+        await bot.send_message(
+            message.from_user.id,
+            f"{animation['title']}\n"
+            f"Год: {desc[0]}\n"
+            f"Город: {desc[1]}\n"
+            f"Жанр: #{desc[2]}\n\n"
+            f"{animation['link']}"
+         )
+
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(quiz_1, commands=["quiz"], commands_prefix="!")
     dp.register_message_handler(command_mem, commands=["mem"], commands_prefix="!")
     dp.register_message_handler(pin, commands=["pin"], commands_prefix="!")
+    dp.register_message_handler(parser_series, commands='series', commands_prefix='/')
+    dp.register_message_handler(parser_cartoons, commands='cartoons', commands_prefix='/')
+    dp.register_message_handler(parser_anime, commands='anime', commands_prefix='/')
