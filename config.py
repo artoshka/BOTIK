8193e5423283167de471009bd68d43d3d630538e
artoshka <105043407+artoshka@users.noreply.github.com>
2022-07-01 12:47:35 +0300

    fix error

---
+++ config.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from decouple import config
+
+storage = MemoryStorage()
+
+
+
+
+TOKEN = config("TOKEN")
+bot = Bot(TOKEN)
+dp = Dispatcher(bot=bot, storage=storage)
+ADMIN = [5215798091]
